e2be3ab7b7b9133cd662d8648c2f495f4cb49302
mabul <hhon0529@naver.com>
2022-06-11 01:57:26 +0900

    Link태그 적용

---
+++ src/main/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./index.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://b3c64ce2-a912-4b9f-9b02-b8f01b9249c1.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/images/banners/banner1.png" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <Link className="product-Link" to={`/products/ ${index}`}>
+                  <div>
+                    <img className="product-img" src={product.imageUrl} />
+                  </div>
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
+export default MainPage;
